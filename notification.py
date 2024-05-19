e86b114223a1bd6d8fedd04f2f823f72584db651
Abdul Nayeem <154670886+abdul6966@users.noreply.github.com>
2024-05-19 14:23:55 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,1 +1,10 @@
+from plyer import notification
 
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title="Did you drink enough water today?",
+            message="About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+            app_icon=r"C:\Users\Abdul Nayeem\Desktop\Python projects\notification\icon.ico",
+            timeout=6
+        )
